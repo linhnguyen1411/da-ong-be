6c6c58dec10486526729930377c267a9858c1941
Linh Nguyen <linhnguyen21@192.168.2.8>
2025-12-27 16:52:29 +0700

    Fix health controller - remove invalid skip_before_action

--- app/controllers/api/v1/health_controller.rb
+++ app/controllers/api/v1/health_controller.rb
@@ -1,8 +1,6 @@
 module Api
   module V1
     class HealthController < ApplicationController
-      skip_before_action :verify_authenticity_token, if: :verify_authenticity_token
-
       def index
         checks = {
           status: 'ok',
